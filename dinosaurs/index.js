092372a4fb52807594591abd7128e01606baeefd
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-12-21 20:58:20 +0100

    moment.js

---
+++ dinosaurs/index.js
@@ -0,0 +1,8 @@
+// index.js  
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+
+(function() {
+    // This is an IIFE
+    console.log("Hello, World!");
+})();
