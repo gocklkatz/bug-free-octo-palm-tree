321177f001567a322acea3b7ce7226b540ad3c2e
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2025-01-06 17:00:21 +0100

    Typescript Basics 1

---
+++ 2_max_stuff/chp2_typescript_basics/app.js
@@ -0,0 +1,12 @@
+//console.log('Your code goes here...'); 
+// Core Types
+//   number 1, 5.3, -10
+//   string 'hi', "hi", `hi`
+//   boolean true, false
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
---
+++ 2_max_stuff/chp2_typescript_basics/app.ts
@@ -0,0 +1,16 @@
+//console.log('Your code goes here...'); 
+
+// Core Types
+//   number 1, 5.3, -10
+//   string 'hi', "hi", `hi`
+//   boolean true, false
+
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
